f33753fda02de035926cdbb5d650329326936c06
ouyen <ouyben@foxmail.com>
2016-06-15 10:35:32 +0800

    添加swipeRefreshLayout自动刷新.

---
+++ androidutils/src/main/java/com/ouyben/androidutils/recyclerview/AutoSwipeRefreshLayout.java
@@ -0,0 +1,42 @@
+package com.ouyben.androidutils.recyclerview;
+
+import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * TODO : SwipeRefreshLayout添加自动刷新
+ * Created by owen on 2016-06-15.
+ */
+public class AutoSwipeRefreshLayout extends SwipeRefreshLayout {
+    public AutoSwipeRefreshLayout(Context context) {
+        super(context);
+    }
+
+    public AutoSwipeRefreshLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    /**
+     * 自动刷新
+     */
+    public void autoRefresh() {
+        try {
+            Field mCircleView = SwipeRefreshLayout.class.getDeclaredField("mCircleView");
+            mCircleView.setAccessible(true);
+            View progress = (View) mCircleView.get(this);
+            progress.setVisibility(VISIBLE);
+
+            Method setRefreshing = SwipeRefreshLayout.class.getDeclaredMethod("setRefreshing", boolean.class, boolean.class);
+            setRefreshing.setAccessible(true);
+            setRefreshing.invoke(this, true, true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
