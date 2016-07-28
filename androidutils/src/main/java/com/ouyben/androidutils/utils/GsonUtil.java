4008faf9730d49cbef161cd9ef0a82cc47c79399
ouyen <ouyben@foxmail.com>
2016-07-28 17:14:41 +0800

    优化首选项保存工具类

--- androidutils/src/main/java/com/ouyben/androidutils/utils/GsonUtil.java
+++ androidutils/src/main/java/com/ouyben/androidutils/utils/GsonUtil.java
@@ -1,91 +1,105 @@
 package com.ouyben.androidutils.utils;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-/**
- * @Description Gson工具类
- */
+
 public class GsonUtil {
 
-    private static volatile GsonUtil sGsonUtil;
-    private Gson mGson;
-
-    public static GsonUtil getGsonUtil() {
-        if (sGsonUtil == null) {
-            synchronized (GsonUtil.class) {
-                if (sGsonUtil == null)
-                    sGsonUtil = new GsonUtil();
-            }
-        }
-        return sGsonUtil;
-    }
-
     private GsonUtil() {
-        mGson = new Gson();
+        /* cannot be instantiated */
+        throw new UnsupportedOperationException("cannot be instantiated");
     }
 
-    public <T> T getBean(String jsonString, Class<T> cls) {
-        T t = null;
-        try {
-            if (mGson != null)
-                t = mGson.fromJson(jsonString, cls);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    /**
+     * 转成json
+     *
+     * @param object
+     * @return
+     */
+    public static String GsonString(Object object) {
+        String gsonString = new Gson().toJson(object);
+        return gsonString;
+    }
+
+    /**
+     * 转成bean
+     *
+     * @param gsonString
+     * @param cls
+     * @return
+     */
+    public static <T> T GsonToBean(String gsonString, Class<T> cls) {
+        T t = new Gson().fromJson(gsonString, cls);
         return t;
     }
 
-    public <T> List<T> getBeans(String jsonString, Class<T> cls) {
+    /**
+     * 转成list
+     * 泛型在编译期类型被擦除导致报错
+     *
+     * @param gsonString
+     * @param cls
+     * @return
+     */
+    public static <T> List<T> GsonToList(String gsonString, Class<T> cls) {
+        List<T> list = null;
+        list = new Gson().fromJson(gsonString, new TypeToken<List<T>>() {
+        }.getType());
+        return list;
+    }
+
+    /**
+     * 转成list
+     * 解决泛型问题
+     *
+     * @param json
+     * @param cls
+     * @param <T>
+     * @return
+     */
+    public <T> List<T> jsonToList(String json, Class<T> cls) {
+        Gson gson = new Gson();
         List<T> list = new ArrayList<T>();
-        try {
-            if (mGson != null)
-                list = mGson.fromJson(jsonString, new TypeToken<List<T>>() {
+        JsonArray array = new JsonParser().parse(json).getAsJsonArray();
+        for (final JsonElement elem : array) {
+            list.add(gson.fromJson(elem, cls));
+        }
+        return list;
+    }
+
+
+    /**
+     * 转成list中有map的
+     *
+     * @param gsonString
+     * @return
+     */
+    public static <T> List<Map<String, T>> GsonToListMaps(String gsonString) {
+        List<Map<String, T>> list = null;
+        list = new Gson().fromJson(gsonString,
+                new TypeToken<List<Map<String, T>>>() {
                 }.getType());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         return list;
     }
 
-    public <T> List<T> getBeans2(String jsonString, Class<T[]> cls) {
-        T[] ts = null;
-        try {
-            if (mGson != null) {
-                ts = mGson.fromJson(jsonString, cls);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return Arrays.asList(ts);
-    }
-
-    public List<String> getList(String jsonString) {
-        List<String> list = new ArrayList<String>();
-        try {
-            list = mGson.fromJson(jsonString, new TypeToken<List<String>>() {
-            }.getType());
-        } catch (Exception e) {
-            // TODO: handle exception  
-        }
-        return list;
-
-    }
-
-    public List<Map<String, Object>> listKeyMap(String jsonString) {
-        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-        try {
-            list = mGson.fromJson(jsonString, new TypeToken<List<Map<String, Object>>>() {
-            }.getType());
-        } catch (Exception e) {
-            // TODO: handle exception  
-        }
-        return list;
+    /**
+     * 转成map的
+     *
+     * @param gsonString
+     * @return
+     */
+    public static <T> Map<String, T> GsonToMaps(String gsonString) {
+        Map<String, T> map = null;
+        map = new Gson().fromJson(gsonString, new TypeToken<Map<String, T>>() {
+        }.getType());
+        return map;
     }
 }
--- app/src/main/java/com/ouyben/oybutils/Main2Activity.java
+++ app/src/main/java/com/ouyben/oybutils/Main2Activity.java
@@ -1,13 +1,75 @@
 package com.ouyben.oybutils;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTextView;
+    private TextView mTextView2;
+    private TextView mTextView3;
+    private TextView mTextView4;
+    private Button mButton;
+    private Button mButton2;
+    private EditText mEditText;
+    private EditText mEditText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        initView();
+    }
+
+
+    private void initView() {
+        mTextView = (TextView) findViewById(R.id.textView);
+        mTextView2 = (TextView) findViewById(R.id.textView2);
+        mTextView3 = (TextView) findViewById(R.id.textView3);
+        mTextView4 = (TextView) findViewById(R.id.textView4);
+        mButton = (Button) findViewById(R.id.button);
+        mButton2 = (Button) findViewById(R.id.button2);
+        mEditText = (EditText) findViewById(R.id.editText);
+        mEditText2 = (EditText) findViewById(R.id.editText2);
+
+        mButton.setOnClickListener(this);
+        mButton2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+
+                break;
+            case R.id.button2:
+
+                break;
+        }
+    }
+
+    private void submit() {
+        // validate
+        String editTextString = mEditText.getText().toString().trim();
+        if (TextUtils.isEmpty(editTextString)) {
+            Toast.makeText(this, "请输入手机号码", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String editText2String = mEditText2.getText().toString().trim();
+        if (TextUtils.isEmpty(editText2String)) {
+            Toast.makeText(this, "请输入你的生日", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // TODO validate success, do something
+
+
     }
 }
